68e0ca35a078bc789b0e928e57abab57a39d92e5
Dragos-Adrian Seredinschi <dragos-adrian.seredinschi@epfl.ch>
2014-12-03 00:11:48 +0100

    Playing with signal handlers.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,36 @@
 module Main where
 
 
+import System.Posix.Signals (installHandler, Handler(Catch), sigINT, sigTERM)
+import Control.Concurrent.MVar (tryTakeMVar, MVar, newEmptyMVar, putMVar)
+import Control.Concurrent (threadDelay)
+import System.IO (hFlush, stdout)
+
+
+handler :: MVar (Int) -> IO ()
+handler s_interrupted = do
+    putStrLn "Caught!"
+    putMVar s_interrupted 1
+
+
 main :: IO ()
-main = putStrLn "hello"
+main = do
+    s_interrupted <- newEmptyMVar
+    _ <- installHandler sigINT (Catch $ handler s_interrupted) Nothing
+    _ <- installHandler sigTERM (Catch $ handler s_interrupted) Nothing
+    putStrLn "heueue"
+    recvFunction s_interrupted
+
+      
+recvFunction :: MVar (Int) -> IO ()
+recvFunction mi = do
+    putStrLn "Waiting.. "
+    threadDelay 1000000
+    hFlush stdout
+    mv <- tryTakeMVar mi
+    case mv of 
+        Just val -> do
+            putStrLn $ "W: Interrupt Received. Killing Server" ++ (show val)
+            hFlush stdout
+            return ()
+        Nothing -> recvFunction mi
